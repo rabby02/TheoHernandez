e40e891ff7bc942956cf7e24d3f5f0b3bed94d5e
Yasin Rabby <56226067+rabby02@users.noreply.github.com>
2023-04-25 22:59:51 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package FreeCrmTestAutomation.FreMidtrans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
